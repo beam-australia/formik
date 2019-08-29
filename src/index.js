0dd3f5f8325db39926108d13b304d011d4d584a4
Andrew McLagan <andrewmclagan@gmail.com>
2019-08-29 10:17:53 +1000

    Dropdown

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import Checkbox from 'fields/Checkbox'
-import CheckboxGroup from 'fields/CheckboxGroup'
-import ConditionalSelect from 'fields/ConditionalSelect'
-import DateSelect from 'fields/DateSelect'
-import MultiSelect from 'fields/MultiSelect'
-import Password from 'fields/Password'
-import RadioGroup from 'fields/RadioGroup'
-import Select from 'fields/Select'
-import Switch from 'fields/Switch'
-import TextField from 'fields/TextField'
-import Editor from 'fields/Editor'
-import Upload from 'fields/Upload'
+import Checkbox from "fields/Checkbox";
+import CheckboxGroup from "fields/CheckboxGroup";
+import ConditionalSelect from "fields/ConditionalSelect";
+import DateSelect from "fields/DateSelect";
+import MultiSelect from "fields/MultiSelect";
+import Password from "fields/Password";
+import RadioGroup from "fields/RadioGroup";
+import Select from "fields/Select";
+import Switch from "fields/Switch";
+import TextField from "fields/TextField";
+import Editor from "fields/Editor";
+import Upload from "fields/Upload";
 
-import FormControl from 'components/FormControl'
-import FormStatus from 'components/FormStatus'
-import SubmitButton from 'components/SubmitButton'
+import FormControl from "components/FormControl";
+import FormStatus from "components/FormStatus";
+import SubmitButton from "components/SubmitButton";
+import Dropdown from "components/Select";
+import Options from "components/Options";
 
-import Handlers from 'lib/Handlers'
-import EditorState from 'lib/EditorState'
+import Handlers from "lib/Handlers";
+import EditorState from "lib/EditorState";
 
 export {
   Checkbox,
@@ -35,5 +37,7 @@ export {
   FormStatus,
   SubmitButton,
   Handlers,
-  EditorState
-}
+  EditorState,
+  Options,
+  Dropdown
+};
